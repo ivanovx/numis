5c565771e80e83284dc8bb620bbcf75b0e426b74
Ivan Ivanov <me@ivanov.pro>
2026-05-29 23:39:19 +0300

    clear project

--- app/src/composables/useSettings.js
+++
@@ -1,6 +0,0 @@
-import { useLocalStorage } from "@vueuse/core";
-
-export const imagePresentation = useLocalStorage('image-presentation', 'image')
-export const statusPresentation = useLocalStorage('status-presentation', 'text_icon')
-export const currentTheme = useLocalStorage('theme', 'light')
-export const apiKey = useLocalStorage('apiKey', null )
